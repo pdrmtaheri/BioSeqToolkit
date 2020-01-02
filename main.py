b63d622d0ebdfb4c04d4e13f82e4e04c3af0a245
Pedram Taheri <pdrm.taheri@gmail.com>
2020-01-02 17:29:14 +0330

    Add InputDialog class

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import tkinter as tk
 
+from src.commons import InputDialog
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.pattern_finder = tk.Button(self)
+        self.pattern_finder['text'] = 'Pattern Finder'
+        self.pattern_finder['command'] = self.create_pattern_finder_window
+        self.pattern_finder.pack(side='top')
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
+    def create_pattern_finder_window(self):
+        print(InputDialog(master=self).show())
 
 
 root = tk.Tk()
---
+++ src/commons.py
@@ -0,0 +1,56 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+
+class InputDialog(object):
+    def __init__(self, master):
+        self.toplevel = tk.Toplevel(master)
+
+        self.input_txt = self.create_input()
+        self.browse_btn = self.create_browse_button()
+        self.done_btn = self.create_done_button()
+
+        self.data_file = None
+
+    def create_input(self):
+        input_txt = tk.Text(self.toplevel)
+        input_txt.pack(side='top')
+
+        return input_txt
+
+    def create_browse_button(self):
+        browse_btn = tk.Button(self.toplevel)
+        browse_btn['text'] = 'Choose File'
+        browse_btn['command'] = self.open_choose_file_dialog
+        browse_btn.pack(side='top')
+
+        return browse_btn
+
+    def create_done_button(self):
+        done_btn = tk.Button(self.toplevel)
+        done_btn['text'] = 'Done'
+        done_btn['command'] = self.done
+        done_btn.pack(side='top')
+
+        return done_btn
+
+    def done(self):
+        if self.data_file is not None:
+            self.data = self.data_file.read()
+        else:
+            self.data = self.input_txt.get(1.0, 'end')
+
+        self.toplevel.destroy()
+
+    def open_choose_file_dialog(self):
+        filename = filedialog.askopenfilename(parent=self.toplevel)
+        try:
+            self.data_file = open(filename, 'r')
+        except FileNotFoundError:
+            print('File not found')
+
+    def show(self):
+        self.toplevel.deiconify()
+        self.toplevel.wait_window()
+        return self.data
