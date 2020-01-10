12cf292f333cdd63eb3d6fed397c28c2cd65f0df
ata <ata.kamal95@gmail.com>
2020-01-11 00:32:59 +0330

    Implement run function for LongestRepetitiveSubstringFinder class

--- src/longest_repetitive_substring_finder.py
+++ src/longest_repetitive_substring_finder.py
@@ -1,6 +1,21 @@
 import tkinter as tk
 
+from suffix_tree import Tree
+
 from src.input import InputDialog
+
+internal_nodes = {}
+
+
+def compute_internals(node):
+    global internal_nodes
+    if node.is_leaf():
+        internal_nodes[node] = 0
+        return
+    internals = 1
+    for child in node.children.values():
+        internals += internal_nodes[child]
+    internal_nodes[node] = internals
 
 
 class LongestRepetitiveSubstringFinder(tk.Frame):
@@ -30,3 +45,9 @@ class LongestRepetitiveSubstringFinder(tk.Frame):
 
     def run(self):
         self.load_k()
+        tree = Tree({1: self.sequence})
+        tree.root.post_order(compute_internals)
+        candidate_nodes = list(filter(lambda node: internal_nodes[node] + 1 >= self.k, internal_nodes))
+        candidate_strings = list(map(lambda node: str(node.path), candidate_nodes))
+        candidate_strings = list(map(lambda string: string.translate(str.maketrans('', '', ' \n$')), candidate_strings))
+        print(max(candidate_strings, key=lambda string: len(string)))
